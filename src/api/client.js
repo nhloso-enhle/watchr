fec897dd37590c22f5b1765d8908ad3feafa2fd3
nhloso-enhle <nhloso.enhle2002@gmail.com>
2026-06-15 22:09:47 +0200

    update .env

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://watchr.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 const client = axios.create({ baseURL: API_URL });
 
 client.interceptors.request.use((config) => {
